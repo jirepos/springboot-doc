727544ef1fa0650c4c00b182c3e2d2361a3a2c8e
jirepos <jirepos@gmail.com>
2023-05-19 17:32:43 +0900

    프로젝트 초기설정

---
+++ src/main/java/com/jirepos/autoconfig/controller/JspController.java
@@ -0,0 +1,15 @@
+package com.jirepos.autoconfig.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jsp")
+public class JspController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "/index";
+    }
+
+}
---
+++ src/main/webapp/resources/main.js
@@ -0,0 +1,2 @@
+
+var programId = "main";
